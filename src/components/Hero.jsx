c97e6ba03058519fc523796f0a23867b262b3f13
huytdn <saolailatoivay991@gmail.com>
2025-09-26 23:44:51 +0700

    hero + category

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,154 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import headset from "../assets/Images/headset.jpg";
+import earbuds from "../assets/Images/earbuds.jpg";
+import dslr from "../assets/Images/dslr.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrowsL: false,
+  };
+
+  useEffect(() => {
+    AOS.init({ offset: 100, duration: 500, easing: "ease-in-out" });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div
+      id="hero"
+      className="w-full flex justify-center items-center lg:h-[700px] h-[600px]"
+    >
+      <Slider className="w-full" {...settings}>
+        <div>
+          <div
+            className="w-full lg:px-20 px-5 lg:h-[700px] h-[600px] flex flex-col justify-center items-start gap-10 bg-cover bg-center"
+            style={{ backgroundImage: `url(${dslr})` }}
+          >
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="50"
+              className="text-themeyellow border rounded-lg border-themeyellow px-6 py-2 text-xl"
+            >
+              Get up to Discounts 80% Off
+            </h1>
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white lg:text-[120px] text-[60px] uppercase font-bold lg:leading-[120px] leading-[70px]"
+            >
+              DSLR 360 <br />
+              Camera
+            </h1>
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white text-2xl"
+            >
+              100% trusted
+              <span className="text-themeyellow font-semibold">
+                Electronics Gadgets
+              </span>
+            </h1>
+            <button
+              data-aos="zoom-in"
+              data-aos-delay="200"
+              className="bg-themeyellow px-6 py-3 rounded-lg text-black font-semibold"
+            >
+              ONLINE COLLECTIONS
+            </button>
+          </div>
+        </div>
+        <div>
+          <div
+            className="w-full lg:px-20 px-5 lg:h-[700px] h-[600px] flex flex-col justify-center items-start gap-10 bg-cover bg-center"
+            style={{ backgroundImage: `url(${earbuds})` }}
+          >
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="50"
+              className="text-themeyellow border rounded-lg border-themeyellow px-6 py-2 text-xl"
+            >
+              Get up to Discounts 80% Off
+            </h1>
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white lg:text-[120px] text-[60px] uppercase font-bold lg:leading-[120px] leading-[70px]"
+            >
+              Wireless <br />
+              Earbuds
+            </h1>
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white text-2xl"
+            >
+              100% trusted
+              <span className="text-themeyellow font-semibold">
+                Electronics Gadgets
+              </span>
+            </h1>
+            <button
+              data-aos="zoom-in"
+              data-aos-delay="200"
+              className="bg-themeyellow px-6 py-3 rounded-lg text-black font-semibold"
+            >
+              ONLINE COLLECTIONS
+            </button>
+          </div>
+        </div>
+        <div>
+          <div
+            className="w-full lg:px-20 px-5 lg:h-[700px] h-[600px] flex flex-col justify-center items-start gap-10 bg-cover bg-center"
+            style={{ backgroundImage: `url(${headset})` }}
+          >
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="50"
+              className="text-themeyellow border rounded-lg border-themeyellow px-6 py-2 text-xl"
+            >
+              Get up to Discounts 80% Off
+            </h1>
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white lg:text-[120px] text-[60px] uppercase font-bold lg:leading-[120px] leading-[70px]"
+            >
+              Bluetooth <br />
+              Headset
+            </h1>
+            <h1
+              data-aos="zoom-in"
+              data-aos-delay="100"
+              className="text-white text-2xl"
+            >
+              100% trusted
+              <span className="text-themeyellow font-semibold">
+                Electronics Gadgets
+              </span>
+            </h1>
+            <button
+              data-aos="zoom-in"
+              data-aos-delay="200"
+              className="bg-themeyellow px-6 py-3 rounded-lg text-black font-semibold"
+            >
+              ONLINE COLLECTIONS
+            </button>
+          </div>
+        </div>
+      </Slider>
+    </div>
+  );
 };
 
 export default Hero;
--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss({
-      theme: {
-        extend: {
-          colors: {
-            themeyellow: "#f5e60d",
-            themepurple: "#502ec3",
-          },
-        },
-      },
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
 });
